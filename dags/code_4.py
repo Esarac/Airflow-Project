21ccb0ff317b2d66f0560242f6d913b1381b636f
Esteban Ariza Acosta <48232743+Esarac@users.noreply.github.com>
2023-11-19 00:01:17 -0500

    Groups :+1:

--- dags/code_4.py
+++ dags/code_4.py
@@ -2,8 +2,8 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.subdag import SubDagOperator
 
-from subdags.code_4_1 import subdag_downloads
-from subdags.code_4_2 import subdag_transforms
+from groups.code_4_1 import download_tasks
+from groups.code_4_2 import transform_tasks
  
 from datetime import datetime
  
@@ -15,21 +15,13 @@ with DAG(
 ) as dag:
     args = dag.__dict__
     
-    download_files_dag_id = 'download_files'
-    download_files = SubDagOperator(
-        task_id = download_files_dag_id,
-        subdag=subdag_downloads(dag.dag_id, download_files_dag_id, args)
-    )
+    download_files = download_tasks()
     
     check_files = BashOperator(
         task_id='check_files',
         bash_command='sleep 10'
     )
 
-    transform_files_dag_id = 'transform_files'
-    transform_files = SubDagOperator(
-        task_id = transform_files_dag_id,
-        subdag=subdag_transforms(dag.dag_id, transform_files_dag_id, args)
-    )
+    transform_files = transform_tasks()
 
     download_files >> check_files >> transform_files
--- dags/groups/code_4_2.py
+++ dags/groups/code_4_2.py
@@ -1,13 +1,11 @@
-from airflow import DAG
+from airflow.utils.task_group import TaskGroup
 from airflow.operators.bash import BashOperator
 
-def subdag_transforms(parent_dag_id, child_dag_id, args):
-    with DAG(
-        f"{parent_dag_id}.{child_dag_id}",
-        start_date=args['start_date'],
-        schedule_interval=args['schedule_interval'],
-        catchup=args['catchup'],
-    ) as dag:
+def transform_tasks():
+    with TaskGroup(
+        "transforms",
+        tooltip="transform tasks"
+    ) as group:
         
         transform_a = BashOperator(
             task_id='transform_a',
@@ -24,4 +22,4 @@ def subdag_transforms(parent_dag_id, child_dag_id, args):
             bash_command='sleep 10'
         )
         
-        return dag
+        return group
