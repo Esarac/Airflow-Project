348d8aa2bcb153b3a1bdb7ffc164871f2372adad
Esteban Ariza Acosta <48232743+Esarac@users.noreply.github.com>
2023-11-19 00:17:17 -0500

    XCom

---
+++ dags/code_5.py
@@ -0,0 +1,36 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+from airflow.models import TaskInstance
+ 
+from datetime import datetime
+ 
+def _t1(ti: TaskInstance):
+    ti.xcom_push(key='xcom_key', value=24)
+ 
+def _t2(ti: TaskInstance):
+    print(ti.xcom_pull(key='xcom_key', task_ids='t1'))
+ 
+with DAG(
+    dag_id="xcom_dag",
+    start_date=datetime(2022, 1, 1), 
+    schedule_interval='@daily',
+    catchup=False,
+) as dag:
+ 
+    t1 = PythonOperator(
+        task_id='t1',
+        python_callable=_t1
+    )
+ 
+    t2 = PythonOperator(
+        task_id='t2',
+        python_callable=_t2
+    )
+ 
+    t3 = BashOperator(
+        task_id='t3',
+        bash_command="echo ''"
+    )
+ 
+    t1 >> t2 >> t3
